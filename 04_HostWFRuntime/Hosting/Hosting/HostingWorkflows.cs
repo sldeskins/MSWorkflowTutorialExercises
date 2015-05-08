cfbcb39acd3ab9b354b483b119d961ca8c07b518
sldeskins <sldeskins@gmail.com>
2015-05-08 18:27:35 -0400

    Exercise 2: Run a Workflow

---
+++ 04_HostWFRuntime/Hosting/Hosting/HostingWorkflows.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+
+
+namespace Hosting
+{
+   public sealed class HostingWorkflows : SequentialWorkflowActivity
+   {
+       private DelayActivity delayActivity1;
+       private CodeActivity codeActivity2;
+       private CodeActivity codeActivity1;
+
+       public HostingWorkflows ()
+       {
+           InitializeComponent();
+       }
+
+       private void InitializeComponent ()
+       {
+           this.CanModifyActivities = true;
+
+           this.codeActivity1 = new System.Workflow.Activities.CodeActivity();
+           this.delayActivity1 = new System.Workflow.Activities.DelayActivity();
+           this.codeActivity2 = new System.Workflow.Activities.CodeActivity();
+
+           // 
+           // codeActivity1
+           // 
+           this.codeActivity1.Name = "codeActivity1";
+           this.codeActivity1.ExecuteCode +=
+               new System.EventHandler(this.codeActivity1_ExecuteCode);
+           // 
+           // delayActivity1
+           // 
+           this.delayActivity1.Name = "delayActivity1";
+           this.delayActivity1.TimeoutDuration = System.TimeSpan.Parse("00:00:05");
+           // 
+           // codeActivity2
+           // 
+           this.codeActivity2.Name = "codeActivity2";
+           this.codeActivity2.ExecuteCode +=
+               new System.EventHandler(this.codeActivity2_ExecuteCode);
+
+
+           // 
+           // HostingWorkflows
+           // 
+           this.Activities.Add(this.codeActivity1);
+           this.Activities.Add(this.delayActivity1);
+           this.Activities.Add(this.codeActivity2);
+           this.Name = "HostingWorkflows";
+
+           this.CanModifyActivities = false;
+       }
+
+      
+       private void codeActivity1_ExecuteCode ( object sender, EventArgs e )
+       {
+           Console.WriteLine("In codeActivity1_ExecuteCode ");
+       }
+       private void codeActivity2_ExecuteCode ( object sender, EventArgs e )
+       {
+           Console.WriteLine("In codeActivity2_ExecuteCode ");
+       }
+
+    }
+}
---
+++ 04_HostWFRuntime/Hosting/Hosting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Workflow.Runtime;
+using System.Workflow.Runtime.Hosting;
+
+namespace Hosting
+{
+    class Program
+    {
+        static AutoResetEvent waitHandle = new AutoResetEvent(false);
+
+        static void Main ( string[] args )
+        {
+            WorkflowRuntime workflowRuntime = new WorkflowRuntime();
+            workflowRuntime.StartRuntime();
+            workflowRuntime.WorkflowCompleted += new
+                EventHandler<WorkflowCompletedEventArgs>
+                (workflowRuntime_WorkflowCompleted);
+
+            Type type = typeof(HostingWorkflows);
+            WorkflowInstance workflowInstance = workflowRuntime.CreateWorkflow(type);
+            workflowInstance.Start();
+
+            waitHandle.WaitOne();
+            workflowRuntime.StopRuntime();
+        }
+        static void workflowRuntime_WorkflowCompleted ( object sender,
+    WorkflowCompletedEventArgs e )
+        {
+            waitHandle.Set();
+        }
+    }
+}
